7ab7238531fa7d8258017cce1db6d08f1d8c2724
LauraPeric <75144169+LauraPeric@users.noreply.github.com>
2024-04-16 17:11:53 +0200

    update

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
-
+import axios from 'axios';
 
 createApp(App).use(router).mount('#app')
+
+axios.get('https://projektwebapps-backend-1.onrender.com')
+    .then(response => {
+        // Handle successful response
+        console.log(response.data);
+    })
+    .catch(error => {
+        // Handle error
+        console.error('Error fetching data:', error);
+    });
